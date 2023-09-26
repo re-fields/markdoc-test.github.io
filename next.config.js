59b88edd08c38a7751c0c6e8669012aa17dddb6b
Renato Campos <renato.campos@ntconsult.com.br>
2023-09-26 17:48:40 -0300

    fix image in build

--- next.config.js
+++ next.config.js
@@ -10,5 +10,8 @@ const withMarkdoc = require("@markdoc/next.js")
 module.exports = withMarkdoc()({
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx']
+  pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx'],
+  images: {
+    unoptimized: true,
+  },
 })
